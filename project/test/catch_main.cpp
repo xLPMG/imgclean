e594deee0de35abe10726332a6b4d7a1e8cfa89e
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2025-12-01 17:10:15 +0100

    init project

---
+++ project/test/catch_main.cpp
@@ -0,0 +1,3 @@
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_FAST_COMPILE // for faster recompilation
+#include "catch.hpp"
---
+++ project/test/example_test.cpp
@@ -0,0 +1,6 @@
+#include <catch2/catch.hpp>
+
+TEST_CASE("Example test case", "[example]")
+{
+    REQUIRE(1 + 1 == 2);
+}
