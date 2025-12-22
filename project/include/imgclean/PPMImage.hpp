74dcca8cffeb4d186b6c05dd81c4760e480a3bd1
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2025-12-23 00:42:37 +0100

    feat: preserve EXIF data in JPEG (#1)

--- project/include/imgclean/PPMImage.hpp
+++ project/include/imgclean/PPMImage.hpp
@@ -14,11 +14,13 @@ struct PPMImage
 	int height = 0;
 	int maxval = 255;
 	std::vector<uint16_t> pixels; // size = width * height * 3 (RGB)
+	std::vector<unsigned char> exif_data; // EXIF segment for JPG
 
 	bool empty() const { return width <= 0 || height <= 0 || pixels.empty(); }
 	void clear()
 	{
 		pixels.clear();
+		exif_data.clear();
 		width  = 0;
 		height = 0;
 		maxval = 255;
