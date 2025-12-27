b8dff5fff389447ee4cdcb5f98eba50d4a63ac08
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2025-12-27 21:51:02 +0100

    feat: first performance optimization

--- project/src/processors/ImageBinarizationProcessor.cpp
+++ project/src/processors/ImageBinarizationProcessor.cpp
@@ -7,96 +7,100 @@
 namespace imgclean::processors
 {
 
-GSImage ImageBinarizationProcessor::apply(const GSImage &image)
+GSImage ImageBinarizationProcessor::apply(const GSImage& image)
 {
 	if (image.empty()) return GSImage();
 
-	// calculate mean
-	float g_mean = 0;
-	for (uint8_t pixel : image.pixels)
-	{
-		g_mean += pixel;
-	}
-	g_mean /= image.pixels.size();
+	const int width             = image.width;
+	const int height            = image.height;
+	const size_t num_pixels     = image.pixels.size();
+	const unsigned char* pixels = image.pixels.data();
 
-	// calculate min, max window
-	std::vector<float> windows_mean(image.pixels.size());
-	std::vector<float> windows_stddev(image.pixels.size());
+	std::vector<float> windows_mean(num_pixels);
+	std::vector<float> windows_stddev(num_pixels);
 	float w_min_stddev = std::numeric_limits<float>::max();
 	float w_max_stddev = std::numeric_limits<float>::min();
 
-	for (int i = 0; i < image.width; ++i)
+	for (int j = 0; j < height; ++j)
 	{
-		for (int j = 0; j < image.height; ++j)
+		for (int i = 0; i < width; ++i)
 		{
 			int x1 = std::max(0, i - half_window);
 			int y1 = std::max(0, j - half_window);
-			int x2 = std::min(image.width - 1, i + half_window);
-			int y2 = std::min(image.height - 1, j + half_window);
+			int x2 = std::min(width - 1, i + half_window);
+			int y2 = std::min(height - 1, j + half_window);
 
 			float tmp_acc = 0;
-			for ( int x = x1; x <= x2; ++x)
+			for (int y = y1; y <= y2; ++y)
 			{
-				for ( int y = y1; y <= y2; ++y)
+				for (int x = x1; x <= x2; ++x)
 				{
-					tmp_acc += image.pixels[y * image.width + x];
+					tmp_acc += pixels[y * width + x];
 				}
 			}
-			float cur_mean = tmp_acc / ((x2 - x1 + 1) * (y2 - y1 + 1));
-			windows_mean[j * image.width + i] = cur_mean;
+			float cur_mean              = tmp_acc / ((x2 - x1 + 1) * (y2 - y1 + 1));
+			windows_mean[j * width + i] = cur_mean;
 
 			float cur_stddev = 0.0;
-			for (int x = x1; x <= x2; ++x)
+			for (int y = y1; y <= y2; ++y)
 			{
-				for ( int y = y1; y <= y2; ++y)
+				for (int x = x1; x <= x2; ++x)
 				{
-					cur_stddev += (image.pixels[y * image.width + x] - cur_mean) * (image.pixels[y * image.width + x] - cur_mean);
+					float diff = pixels[y * width + x] - cur_mean;
+					cur_stddev += diff * diff;
 				}
 			}
 
 			cur_stddev = std::sqrt(cur_stddev / ((x2 - x1 + 1) * (y2 - y1 + 1)));
 
-			if ( cur_stddev > w_max_stddev )
+			if (cur_stddev > w_max_stddev)
 			{
 				w_max_stddev = cur_stddev;
 			}
 
-			if ( cur_stddev < w_min_stddev )
+			if (cur_stddev < w_min_stddev)
 			{
 				w_min_stddev = cur_stddev;
 			}
 
-			windows_stddev[j * image.width + i] = cur_stddev;
+			windows_stddev[j * width + i] = cur_stddev;
 		}
 	}
 
 	// at this point: g_mean, w_min;max_stddev
 
-	// pepare output
+	// prepare output
 	GSImage output_image;
-	output_image.width  = image.width;
-	output_image.height = image.height;
-	output_image.maxval = image.maxval;
+	output_image.width     = width;
+	output_image.height    = height;
+	output_image.maxval    = image.maxval;
 	output_image.exif_data = image.exif_data;
-	output_image.pixels.resize(image.width * image.height);
+	output_image.pixels.resize(num_pixels);
 
-	// NOW: iterate again
-	for (int i = 0; i < image.width; ++i)
+	const float global_mean = std::accumulate(pixels, pixels + num_pixels, 0.0f) / static_cast<float>(num_pixels);
+
+	// Iterate again for binarization
+	for (int j = 0; j < height; ++j)
 	{
-		for (int j = 0; j < image.height; ++j)
+		for (int i = 0; i < width; ++i)
 		{
+			const size_t index        = j * width + i;
+			const float current_value = windows_stddev[index];
+
 			// adaptive_stddev
 			float adaptive_stddev = 0.0;
 			if (w_max_stddev > w_min_stddev)
 			{
-				adaptive_stddev = (windows_stddev[j * image.width + i] - w_min_stddev) / (w_max_stddev - w_min_stddev);
+				adaptive_stddev = (current_value - w_min_stddev) / (w_max_stddev - w_min_stddev);
 			}
 
 			// threshold calculation
-			float threshold = windows_mean[j * image.width + i] - (windows_mean[j * image.width + i] * windows_mean[j * image.width + i ] - windows_stddev[j * image.width + i]) / ((g_mean + windows_stddev[j * image.width + i]) * (adaptive_stddev + windows_stddev[j * image.width + i]));
+			float threshold = current_value -
+			                  (windows_mean[index] * windows_mean[index] - current_value) /
+			                          ((global_mean + current_value) * (adaptive_stddev + current_value));
 
 			// binarization
-			output_image.pixels[j * image.width + i] = (image.pixels[j * image.width + i] < threshold) ? 0 : 255;
+			output_image.pixels[index] = (pixels[index] < threshold) ? 0 : 255;
 		}
 	}
 
@@ -104,4 +108,3 @@ GSImage ImageBinarizationProcessor::apply(const GSImage &image)
 }
 
 } // namespace imgclean::processors
-
